05058f9f68404a0253b13db22d6dbb6f740d2f84
Vinícius Silva Fernandes Kuhlmann <vinikuhlmann@gmail.com>
2023-01-26 21:39:24 -0300

    Text change

--- src/index.js
+++ src/index.js
@@ -14,10 +14,10 @@ const aboutMeText = (
   <p>
     Olá! Meu nome é Vinícius, tenho 22 anos e sou estudante de Ciência da Computação na Universidade de São Paulo, campus São Carlos,
     com ênfases em Ciência de Dados, Engenharia de Software e Algoritmos Avançados. <br /> <br />
-    Atualmente, estou trabalhando como estagiário na <a href='https: //cpxdigital.com.br/'>CPX Digital</a>, onde desenvolvo programas de web scraping com Selenium,
+    Atualmente, estou trabalhando como estagiário na <a href='https://cpxdigital.com.br/'>CPX Digital</a>, onde desenvolvo programas de web scraping com Selenium,
     e uso Pandas para organizar dados e gerar tabelas. <br /> <br />
-    Estou muito interessado nas áreas de Ciência de Dados, Engenharia de Software e Desenvolvimento Full Stack, então se você tiver alguma oportunidade de trabalho ou estágio, por favor, entre em contato comigo!
-    Aprendo rápido e sou muito dedicado, então acredito que serei um ótimo colaborador para sua empresa. <br /> <br />
+    Me interesso pelas áreas de Ciência de Dados, Engenharia de Software e Desenvolvimento Full Stack.
+    Aprendo rápido e sou muito dedicado; quando gosto de algum assunto, vou a fundo nele. <br /> <br />
   </p>
 )
 
